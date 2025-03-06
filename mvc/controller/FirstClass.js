64c45566e285a1a4e3d3cec9aee57440a59def3f
Reinerth <85163640+Reinerth@users.noreply.github.com>
2025-03-06 19:10:35 +0100

    Update FirstClass.js
    Added to the pattern a sixth file as an example for retrieving content from database with php and the possibility to change the method (POST/GET) on requesting files.

--- mvc/controller/FirstClass.js
+++ mvc/controller/FirstClass.js
@@ -8,29 +8,36 @@
  * from other classes like "SecondClass",
  * even if they are not instantiated at that time,
  * because our container "MyApplicationsName" is global.
+ * Means, we hoisted our container to window,
+ * so the functions are going to be executed 
+ * only after the window is loaded with the complete scripts.
+ * Works similar like the compiled files e.g. in C or JAVA. 
+ * In this way JavaScript is kind of precompiled for the browser.
  * Note: Every class is a file, is a module.
 */
 window.MyApplicationsName.FirstClass = function (){
 
-    // Private
+    // Private (can only be accessed from this class)
     let FirstClassPrivate = function(){
-        console.log("1 FirstClass Private");
+        console.log("Executed in FirstClass Private");
     };
 
-    // Public
-    this.FirstClassPublic = function(){
-        console.log("1 FirstClass Public");
+    // Public (can be accessed from other classes)
+    this.init = function(){
+
+        console.log("Executed in FirstClass Public / init");
+
         FirstClassPrivate();
+
+        mySecC.SecondClassPublic(); // Invoke a public function from ANOTHER class
     };
 };
 
 
 
-
 /****************************************************************
- * INSTANTIATE (new) a Variable (e.g. "myFC")                   *
+ * INSTANTIATE (new) a Variable (e.g. "myFirC")                 *
  * with the class from this file ("FirstClass")                 *
  * to make the class available to be invoked (in the "view.htm")*
 ****************************************************************/
-let myFC = new window.MyApplicationsName.FirstClass();
-
+let myFirC = new window.MyApplicationsName.FirstClass();
